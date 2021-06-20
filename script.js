29294cd5b28f25849d1f1ce9cf32a16ecd2a15a5
Matheus Guilherme Araújo <matheus.guih58@gmail.com>
2021-06-19 22:19:37 -0300

    Finalizando o sidebar

---
+++ script.js
@@ -0,0 +1,6 @@
+const hamburguer = document.querySelector(".hamburguer");
+
+hamburguer.addEventListener("click", function(){
+    document.querySelector(".sidebar").classList.toggle("show-menu");
+
+})
